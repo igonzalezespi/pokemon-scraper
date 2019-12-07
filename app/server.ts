042bef31ae13bfd36e55a7ea1b0863ac0a26f74d
Ivan <inunez@alu.ucam.edu>
2019-12-07 16:03:41 +0100

    Servicios para ataques, objetos, habilidades.

--- app/server.ts
+++ app/server.ts
@@ -3,4 +3,5 @@ import {PokemonService} from "./services/pokemonService";
 (async () => {
     let pokes = await PokemonService.fetchAll();
     console.log(pokes);
+
 })();
---
+++ app/services/attackService.ts
@@ -0,0 +1,6 @@
+export class AttackService {
+    public static url = 'https://www.serebii.net/attackdex-swsh/';
+    public static selectors = {
+        name: 'h1'
+    };
+}
